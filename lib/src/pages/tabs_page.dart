f7f237927146b4e64622cbddf7c405ee6911b7aa
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-21 09:25:59 +0100

    [FEATURE][PROVIDER] * Bottom navigator provider was implemented => https://pub.dev/packages/provider

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,30 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   static String routeName = 'tabs_page';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('News'),
+    return ChangeNotifierProvider(
+      create: (_) => _ModelNavigation(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('News'),
+        ),
+        body: _CustomPageView(),
+        bottomNavigationBar: _CustomButtonNavigationBar(),
       ),
-      body: _buildPageView(),
-      bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
-  BottomNavigationBar _buildBottomNavigationBar() {
-    return BottomNavigationBar(
-      currentIndex: 0,
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Para ti' ),
-        BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Encabezados' ),
-      ],
-    );
-  }
+}
 
-  PageView _buildPageView() {
+class _CustomPageView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
     return PageView(
       // physics: BouncingScrollPhysics(),
       physics: NeverScrollableScrollPhysics(),
@@ -39,3 +38,38 @@ class TabsPage extends StatelessWidget {
     );
   }
 }
+
+class _CustomButtonNavigationBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final _navigationModel = Provider.of<_ModelNavigation>(context);
+    return BottomNavigationBar(
+      currentIndex: _navigationModel.currentPage,
+      onTap: (index) => _navigationModel.setCurrentPage = index,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Para ti' ),
+        BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Encabezados' ),
+      ],
+    );
+  }
+}
+
+class _ModelNavigation with ChangeNotifier {
+  int _currentPage = 0;
+
+  int get currentPage => this._currentPage;
+  set setCurrentPage(int value) {
+    this._currentPage = value;
+    notifyListeners();
+  }
+}
+
+
+
+
+
+
+
+
+
