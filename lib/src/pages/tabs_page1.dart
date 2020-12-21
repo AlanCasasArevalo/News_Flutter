993090a4046b992564bdd9040f10a95f7a879bb2
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-21 12:27:51 +0100

    [FEATURE][WIDGET LIST VIEW]
    * News page was refactored to do not lost scroll point

--- lib/src/pages/tabs_page1.dart
+++ lib/src/pages/tabs_page1.dart
@@ -3,17 +3,27 @@ import 'package:news_flutter_app/src/services/news_service.dart';
 import 'package:news_flutter_app/src/widgets/news_list.dart';
 import 'package:provider/provider.dart';
 
-class TabsPage1 extends StatelessWidget {
+class TabsPage1 extends StatefulWidget {
   static String routeName = 'tabs_page_one';
 
   @override
+  _TabsPage1State createState() => _TabsPage1State();
+}
+
+class _TabsPage1State extends State<TabsPage1> with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
     final headlines = Provider.of<NewsService>(context).headlines;
+    //      body: NewsList( headlines ),
     return Scaffold(
       appBar: AppBar(
         title: Text('News'),
       ),
-      body: NewsList( headlines ),
+      body: (headlines.length == 0)  ? Center( child: CircularProgressIndicator(),) : NewsList( headlines )
+      ,
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
