b9523c59f6a9d6f0318b6b37b2f3450fba3a6268
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-22 16:01:56 +0100

    [FEATURE][CATEGORIES CAROUSEL] * Categories carousel was added with some properties

--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -8,6 +8,8 @@ import 'package:news_flutter_app/src/models/news_models.dart';
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
 
+  String _selectedCategory = 'business';
+
   List<CategoryModel> categories = [
     CategoryModel(FontAwesomeIcons.building, 'business'),
     CategoryModel(FontAwesomeIcons.tv, 'entertainment'),
@@ -18,15 +20,34 @@ class NewsService with ChangeNotifier {
     CategoryModel(FontAwesomeIcons.mobile, 'technology'),
   ];
 
+  // Map<String, List<Article>> categoryArticles = {};
+
   final _baseURL = 'https://newsapi.org/v2';
   final _apiKey = '&apiKey=YOUR_API_KEY';
 
   NewsService() {
     getTopHeadlines();
+    categories.
+  }
+
+  get selectedCategory => this._selectedCategory;
+  set setSelectedCategory (String value) {
+    this._selectedCategory = value;
+    this.getArticlesByCategory(this._selectedCategory);
+    notifyListeners();
+  }
+
+  getArticlesByCategory (String category) async {
+    final url = '$_baseURL/top-headlines?country=co&category=$category$_apiKey';
+    final response = await http.get(url);
+    final newsResponse = newsModelFromJson(response.body);
+
+    this.headlines.addAll(newsResponse.articles);
+    notifyListeners();
   }
 
   getTopHeadlines () async {
-    final url = '$_baseURL/top-headlines?country=co&category=general$_apiKey';
+    final url = '$_baseURL/top-headlines?country=co$_apiKey';
     final response = await http.get(url);
     final newsResponse = newsModelFromJson(response.body);
 
